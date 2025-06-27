0ee4e4ce0a252f7429043781f8b167f746ff3614
Hossam <codingWorld5000@gmail.com>
2025-06-27 22:43:35 +0300

    ne

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
 <template>
   <main>
     <h1>new headline</h1>
+    <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sapiente quo, consectetur exercitationem
+      vitae sint
+      rerum laborum eaque quas, nisi totam aperiam voluptates! Ipsam voluptates nulla fugit numquam. Magnam, explicabo.
+      Unde exercitationem et obcaecati, adipisci asperiores voluptas molestiae ex in cumque? Accusantium temporibus sit
+      natus excepturi, doloribus omnis dignissimos aliquam. Voluptatem, dignissimos. Asperiores explicabo minima error.
+      Quasi vitae officia deleniti.</p>
   </main>
 </template>
 
