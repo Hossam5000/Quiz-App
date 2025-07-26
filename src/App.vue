50ac68da46f3009e30f6a688bc01d6616c689a4f
Hossam <codingWorld5000@gmail.com>
2025-07-26 18:16:15 +0300

    adding the quizes.json file

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,17 @@
     <div class="container">
       <header>
         <h1>Quizes</h1>
-        <input type="text" name="" id="">
+        <input type="text" placeholder="search...">
       </header><!--./header-->
 
       <div class="options-container">
-        options
+        <div class="card">
+          <img src="https://i.ytimg.com/vi/Kp2bYWRQylk/maxresdefault.jpg" alt="Math">
+          <div class="card-text">
+            <h2>Math</h2>
+            <p class="desc">15 questions</p>
+          </div><!--./card-text-->
+        </div><!--./card-->
       </div><!--./options-container-->
     </div><!--./container-->
   </div>
@@ -21,5 +27,56 @@
 .container {
   max-width: 1000px;
   margin: 0 auto;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  margin: 10px 0 30px 0;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+
+  background-color: rgba(128, 128, 128, 0.1);
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-top: 40px;
+}
+
+.card {
+  width: 310px;
+
+  margin: 0 20px 0 35px;
+  border-radius: 2%;
+
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
 }
 </style>
